4159f536f91294f5b8fe70095fd225190ca96d4b
Sven Mkw <sven.mkw@gmail.com>
2015-11-20 11:59:32 +0100

    overlapping test skeleton

--- test/degeneracies.js
+++ test/degeneracies.js
@@ -1,10 +1,29 @@
 var chai = require('chai')
 var expect = chai.expect
 var Polygon = require('../src/polygon')
+var helpers = require('./helpers.js')
+var wrapIntoObject = helpers.wrapIntoObject
 
 describe('Degeneracies Clipping', function() {
 
-  
+  var overlapping = []
+  beforeEach(function() {
+    overlapping = [
+      new Polygon([
+        [-2, -1],
+        [1, -1],
+        [1, 1],
+        [-2, 1]
+      ]),
+      new Polygon([
+        [-1, -1],
+        [2, -1],
+        [2, 1],
+        [-1, 1]
+      ])
+    ]
+  })
+
   describe('#equal', function() {
 
 
@@ -13,7 +32,15 @@ describe('Degeneracies Clipping', function() {
 
   describe('#overlapping', function() {
 
+    it('should union overlapping polygons', function() {
+      var result = overlapping[0].clip(
+        overlapping[1],
+        false,
+        false
+      )
 
+      expect(true).to.be.false
+    })
 
   })
 
