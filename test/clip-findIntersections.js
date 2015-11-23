cdf418a3e3abee161f0993467354a85635645790
dimitri-schmidt <dimitri.schmidt@student.hpi.de>
2015-11-23 16:24:10 +0100

    test degenerated polygons with just one intersection point

--- test/clip-findIntersections.js
+++ test/clip-findIntersections.js
@@ -201,4 +201,62 @@ describe('Find Intersections', function() {
     expect(p2.first.next.next.next._corresponding).to.deep.equal(p1.first.next.next.next.next)
   })
 
+  it('should find intersections for just in one point intersecting degenerated polygons', function() {
+    var p1 = new Polygon([
+      [ -1, 1 ],
+      [  1, 1 ],
+      [  0, 2 ]
+    ])
+    var p2 = new Polygon([
+      [ -1, 0 ],
+      [  1, 0 ],
+      [  0, 1 ]
+    ])
+
+    p1.findIntersections(p2)
+
+    expect(p1.vertices).to.equal(4)
+    expect(p2.vertices).to.equal(3)
+
+    expect(p1.getPoints()).to.deep.equal([
+      [ -1, 1 ],
+      [  0, 1 ],
+      [  1, 1 ],
+      [  0, 2 ]
+    ])
+    expect(p2.getPoints()).to.deep.equal([
+      [ -1, 0 ],
+      [  1, 0 ],
+      [  0, 1 ]
+    ])
+
+    expect(p1.first._isIntersection).to.be.false
+    expect(p1.first.next._isIntersection).to.be.true
+    expect(p1.first.next.next._isIntersection).to.be.false
+    expect(p1.first.next.next.next._isIntersection).to.be.false
+
+    expect(p2.first._isIntersection).to.be.false
+    expect(p2.first.next._isIntersection).to.be.false
+    expect(p2.first.next.next._isIntersection).to.be.true
+
+    expect(p1.first._relativePosition).not.to.equal('on')
+    expect(p1.first.next._relativePosition).to.equal('on')
+    expect(p1.first.next.next._relativePosition).not.to.equal('on')
+    expect(p1.first.next.next.next._relativePosition).not.to.equal('on')
+
+    expect(p2.first._relativePosition).not.to.equal('on')
+    expect(p2.first.next._relativePosition).not.to.equal('on')
+    expect(p2.first.next.next._relativePosition).to.equal('on')
+
+    expect(p1.first._corresponding).to.deep.equal(null)
+    expect(p1.first.next._corresponding).to.deep.equal(p2.first.next.next)
+    expect(p1.first.next.next._corresponding).to.deep.equal(null)
+    expect(p1.first.next.next.next._corresponding).to.deep.equal(null)
+
+    expect(p2.first._corresponding).to.deep.equal(null)
+    expect(p2.first.next._corresponding).to.deep.equal(null)
+    expect(p2.first.next.next._corresponding).to.deep.equal(p1.first.next)
+
+  })
+
 })
